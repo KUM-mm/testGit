00d157ea89ca22d476070cf607e0ba2e71667c0c
KUM <mkw04e@gmail.com>
2020-07-17 18:27:34 +0900

    Test Commit

--- app/src/main/java/org/ku/and/testgit/MainActivity.java
+++ app/src/main/java/org/ku/and/testgit/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String text = "Hi";
+        String text1 = "Next";
     }
 }
